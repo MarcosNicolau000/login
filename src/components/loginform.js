285704165c9ada1ca2009b6bc0f0d341edca49e8
Marcos Nicolau <69830188+oScanner@users.noreply.github.com>
2022-11-15 10:03:55 -0300

    0.0.4 - Corpo do CSS

--- src/components/loginform.js
+++ src/components/loginform.js
@@ -1,11 +1,13 @@
 import React from "react";
-import "./loginform.css"
+import "./loginform.css";
+import img from "./img/loginimg.svg";
 
 const LoginForm = () => {
     return (
         <div className="main-login">
             <div className="left-login">
                 <h1>Entre com a sua conta</h1>
+                <img className="left-login-image" src="./images/loginimg.svg" alt=""></img>
             </div>
             <div className="right-login">
                 <div className="card-login">
@@ -18,8 +20,8 @@ const LoginForm = () => {
                         <label for="senha">Senha</label>
                         <input type="password" name="senha" placeholder="Senha"/>
                     </div>
+                    <button class="btn-login">Login</button>
                 </div>
-                <button class="btn-login">Login</button>
             </div>
         </div>
 
